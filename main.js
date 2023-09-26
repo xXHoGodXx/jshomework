f698435b6a16f436c84d81d4bbcf0a101ad91156
xXHoGodXx <142801872+xXHoGodXx@users.noreply.github.com>
2023-09-26 14:44:35 +0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,15 @@
+let arr  = [4, 16, 19, 22, 11, 144, 967, 19124]
+
+let even = [4,16,22,144,19124];
+
+let odd =[19,11,967];
+
+
+let callBack = arr.filter((arr)=> arr %2===0)
+
+let = (callBack) 
+
+if (callBack) {
+    console.log(callBack);
+}
+ 
